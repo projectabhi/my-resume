e6c93ebb70a8e9139457d33ec63bc2c6a80cc696
projectabhi <projectabhi@gmail.com>
2025-06-09 00:39:41 +0530

    fixed main container render issue

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -1,7 +1,7 @@
 const Container = ({ children }: { children: React.ReactNode }) => {
   return (
     <>
-      <main className="max-w-full h-99 flex flex-col space-y-10 items-center justify-center relative bg-gray-800 text-white">
+      <main className="max-w-full h-0 flex flex-col space-y-10 items-center justify-center relative bg-gray-800 text-white">
         {children}
       </main>
     </>
--- src/pages/MyProfile.tsx
+++ src/pages/MyProfile.tsx
@@ -3,12 +3,16 @@ import Container from "../components/Container";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
 import SideMenu from "../components/SideMenu";
+import backgroundImage from "../assets/background.png";
 
 const MyProfile = () => {
   return (
     <div className="h-screen w-full bg-white relative flex overflow-hidden">
       <SideMenu></SideMenu>
-      <div className="w-full h-full flex flex-col justify-between">
+      <div
+        className="w-full h-full flex flex-col justify-between"
+        style={{ backgroundImage: `url(${backgroundImage})` }}
+      >
         <Header></Header>
         <Container>
           <Outlet />
